a2306fa04383b2acef4a258b9882c5fa420f6686
ian barczewski <ibarczewski@gmail.com>
2021-10-06 22:51:30 -0700

    created ultrawide fix, needs error checking

---
+++ lis-widescreen-unlocker/lis-widescreen-unlocker/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace lis_widescreen_unlocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.chkFovFix = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Patch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(106, 177);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 151);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(220, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 125);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(220, 20);
+            this.txtPath.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1440x900",
+            "1280x1024",
+            "2560x1080",
+            "3440x1440",
+            "3840x1080",
+            "3840x1600",
+            "4120x1024",
+            "5160x1440",
+            "5292x1050",
+            "7680x1440"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(220, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // chkFovFix
+            // 
+            this.chkFovFix.AutoSize = true;
+            this.chkFovFix.Checked = true;
+            this.chkFovFix.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkFovFix.Location = new System.Drawing.Point(71, 214);
+            this.chkFovFix.Name = "chkFovFix";
+            this.chkFovFix.Size = new System.Drawing.Size(92, 17);
+            this.chkFovFix.TabIndex = 5;
+            this.chkFovFix.Text = "Apply FOV Fix";
+            this.chkFovFix.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Step 1: Select A Resolution";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 62);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Step 2: Browse to \"Siren-Win64-Shipping.exe\" - this is found in <InstallPath>/Lif" +
+    "eIsStrange3/Siren/Binaries/Win64";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Step 3: Apply the Patch!";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 272);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkFovFix);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "LiS: True Colors Ultrawide Patcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox chkFovFix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lis-widescreen-unlocker/lis-widescreen-unlocker/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace lis_widescreen_unlocker
+{
+
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> WidescreenOptions = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            WidescreenOptions.Add("1440x900", "CD CC CC 3F");
+            WidescreenOptions.Add("1280x1024", "00 00 A0 3F");
+            WidescreenOptions.Add("2560x1440", "26 B4 17 40");
+            WidescreenOptions.Add("3440x1440", "8E E3 18 40");
+            WidescreenOptions.Add("3840x1080", "39 8E 63 40");
+            WidescreenOptions.Add("3840x1600", "9A 99 19 40");
+            WidescreenOptions.Add("4120x1024", "00 00 A0 3F");
+            WidescreenOptions.Add("5120x1440", "39 8E 63 40");
+            WidescreenOptions.Add("5292x1050", "AE 47 A1 40");
+            WidescreenOptions.Add("7680x1440", "AB AA AA 40");
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var path = txtPath.Text;
+            // var copypath = txtPath.Text.Replace("Siren-Win64-Shipping.exe", "temp.exe");
+            var selected = comboBox1.SelectedItem.ToString().Trim();
+            string resolutionHex = WidescreenOptions[selected];
+
+            byte[] find = ConvertHexStringToByteArray(Regex.Replace("3B 8E E3 3F", "0x |[ ,]", string.Empty).Normalize().Trim());
+            byte[] replace = ConvertHexStringToByteArray(Regex.Replace(resolutionHex, "0x |[ ,]", string.Empty).Normalize().Trim());
+            if (find.Length != replace.Length)
+            {
+                throw new ArgumentException("Find and replace hex must be same length");
+            }
+            byte[] bytes = File.ReadAllBytes(path);
+            var indexes = Search(bytes, find);
+            foreach (int index in indexes)
+            {
+                for (int i = index, replaceIndex = 0; i < bytes.Length && replaceIndex < replace.Length; i++, replaceIndex++)
+                {
+                    bytes[i] = replace[replaceIndex];
+                }
+                
+                
+            }
+
+            if (chkFovFix.Checked)
+            {
+                byte[] findFov = ConvertHexStringToByteArray(Regex.Replace("35 FA 0E 3C AC C5 27 37 6F", "0x |[ ,]", string.Empty).Normalize().Trim());
+                byte[] replaceFov = ConvertHexStringToByteArray(Regex.Replace("35 FA 3E 3C AC C5 27 37 6F", "0x |[ ,]", string.Empty).Normalize().Trim());
+
+                var indexesFov = Search(bytes, findFov);
+                foreach (int index in indexesFov)
+                {
+                    for (int i = index, replaceIndex = 0; i < bytes.Length && replaceIndex < replaceFov.Length; i++, replaceIndex++)
+                    {
+                        bytes[i] = replaceFov[replaceIndex];
+                    }
+
+
+                }
+            }
+
+            File.WriteAllBytes(path, bytes);
+            lblResult.Text = $"Success!";
+        }
+
+        int[] Search(byte[] src, byte[] pattern)
+        {
+            var indexes = new List<int>();
+            int maxFirstCharSlot = src.Length - pattern.Length + 1;
+            for (int i = 0; i < maxFirstCharSlot; i++)
+            {
+                if (src[i] != pattern[0]) // compare only first byte
+                    continue;
+
+                // found a match on first byte, now try to match rest of the pattern
+                for (int j = pattern.Length - 1; j >= 1; j--)
+                {
+                    if (src[i + j] != pattern[j]) break;
+                    if (j == 1) {
+                        indexes.Add(i);
+                    };
+                }
+            }
+            return indexes.ToArray();
+        }
+
+        private static byte[] ConvertHexStringToByteArray(string hexString)
+        {
+            if (hexString.Length % 2 != 0)
+            {
+                throw new ArgumentException(String.Format(CultureInfo.InvariantCulture, "The binary key cannot have an odd number of digits: {0}", hexString));
+            }
+
+            byte[] data = new byte[hexString.Length / 2];
+            for (int index = 0; index < data.Length; index++)
+            {
+                string byteValue = hexString.Substring(index * 2, 2);
+                data[index] = byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            }
+
+            return data;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            string folderPath = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
