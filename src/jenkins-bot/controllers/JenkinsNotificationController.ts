56c89d18cf910cec60a7825a0ce9a85e5a6d9892
Zak Lampert <zlampert@cisco.com>
2021-12-01 09:46:06 -0600

    SPARK-285117 - clean up jenkins controller

---
+++ src/jenkins-bot/controllers/JenkinsNotificationController.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from 'express';
+import { Bot } from '../../common/Bot';
+import { BotFramework } from '../../common/BotFramework';
+import { taskCreatedTemplate } from '../../common/templates';
+import { TaskCreatedTemplateData } from '../../common/templates/TaskCreated';
+
+interface JenkinsNotificationSCM {
+  url: string;
+  branch: string;
+  commit: string;
+}
+
+interface JenkinsNotificationBuild {
+  full_url: string;
+  number: number;
+  phase: 'QUEUED' | 'STARTED' | 'COMPLETED' | 'FINALIZED';
+  status: 'SUCCESS' | 'FAILED';
+  url: string;
+  scm: JenkinsNotificationSCM;
+  artifacts: any; // complex object where keys are probably only known by admin
+}
+
+interface JenkinsNotificationPayload {
+  name: string;
+  url: string;
+  build: JenkinsNotificationBuild;
+}
+
+export class JenkinsNotificationController {
+  execute(
+    req: Request<{ roomId: string }, {}, JenkinsNotificationPayload>,
+    res: Response,
+    framework: BotFramework
+  ) {
+    const bot: Bot = framework.getBotByRoomId(req.params.roomId);
+    if (bot) {
+      try {
+        const { name, build } = req.body;
+        const data: TaskCreatedTemplateData = {
+          projectName: name,
+          title: `Job ${build.phase}`,
+          metadata: [
+            {
+              key: 'Build number:',
+              value: `[${build.number}](${build.full_url})`
+            },
+            { key: 'Status:', value: build.status }
+          ],
+          actions: [
+            {
+              type: 'Action.OpenUrl',
+              title: 'Open in Jenkins',
+              url: build.full_url
+            }
+          ]
+        };
+
+        const jobCompletedCard = taskCreatedTemplate.buildCard(data);
+        bot.sendCard(jobCompletedCard);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.log('could not find bot');
+    }
+
+    res.end();
+  }
+}
--- src/jenkins-bot/index.ts
+++ src/jenkins-bot/index.ts
@@ -1,52 +1,11 @@
-import { Bot } from '../common/Bot';
 import { BotFramework, BotHandler, BotRoute } from '../common/BotFramework';
-import fetch from 'node-fetch';
-import { TaskCreatedTemplateData } from '../common/templates/TaskCreated';
-import { taskCreatedTemplate } from '../common/templates';
-
-const jenkinsEventController = {
-  execute: (req, res, framework) => {
-    const bot: Bot = framework.getBotByRoomId(req.params.roomId);
-    if (bot) {
-      try {
-        const { name, build } = req.body;
-        const data: TaskCreatedTemplateData = {
-          projectName: name,
-          title: `Job ${build.phase}`,
-          metadata: [
-            {
-              key: 'Build number:',
-              value: `[${build.number}](${build.full_url})`
-            },
-            { key: 'Status:', value: build.status }
-          ],
-          actions: [
-            {
-              type: 'Action.OpenUrl',
-              title: 'Open in Jenkins',
-              url: build.full_url
-            }
-          ]
-        };
-
-        const jobCompletedCard = taskCreatedTemplate.buildCard(data);
-        bot.sendCard(jobCompletedCard);
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      console.log('could not find bot');
-    }
-
-    res.end();
-  }
-};
+import { jenkinsNotificationController } from './controllers';
 
 const routes: BotRoute[] = [
   {
     action: 'post',
     path: '/:roomId',
-    controller: jenkinsEventController
+    controller: jenkinsNotificationController
   }
 ];
 
@@ -58,26 +17,9 @@ const handlers: BotHandler[] = [
     }
   },
   {
-    command: 'demo job completed',
+    command: 'get webhook url',
     handler: (bot) => {
-      try {
-        // NOTE: using node-fetch here to use our app end to end (rather than simply sending a card directly)
-        fetch(`${process.env.FRAMEWORK_WEBHOOK_URL}/jenkins/${bot.room.id}`, {
-          method: 'post',
-          body: JSON.stringify({
-            build: {
-              full_url: 'https://google.com',
-              number: 3,
-              phase: 'COMPLETED',
-              status: 'SUCCESS'
-            },
-            name: 'fake jenkins job name'
-          }),
-          headers: { 'Content-Type': 'application/json' }
-        });
-      } catch (e) {
-        console.log(e);
-      }
+      bot.say(`${process.env.FRAMEWORK_WEBHOOK_URL}/jenkins/${bot.room.id}`);
     }
   }
 ];
