b7565684fe112f1d2bc14673e1e2bd53507d127f
ibarczew@cisco.com <ian.barczewski@wwt.com>
2021-12-14 13:02:58 -0600

    287095 - added API service change where builds can access WFAPI endpoints - IB

--- src/jenkins-bot/services/JenkinsAPIService.ts
+++ src/jenkins-bot/services/JenkinsAPIService.ts
@@ -1,32 +1,42 @@
 import fetch from 'node-fetch';
 import { Commit } from '../templates/JobCompletedTemplate';
 
-type JenkinsAPI = (params: { path: string }) => Promise<any>;
+type JenkinsAPI = (params: { path: string; isWfapi: boolean }) => Promise<any>;
 
 interface JenkinsBuildData {
   commits: Commit[];
+}
+
+interface JenkinsStageData {
+  status: string;
+}
+
+interface JenkinsOtherWayBuildData {
+  stages: JenkinsStageData[];
 }
 
 export default class JenkinsRestAPIService {
   private jenkinsAPI: JenkinsAPI;
   private username: string;
   private apiKey: string;
+  private jenkinsUrl: string;
 
   constructor(jenkinsAPISettingsEnvelopeID: string, webex) {
-    this.jenkinsAPI = async ({ path }) => {
-      if (!this.username || !this.apiKey) {
+    this.jenkinsAPI = async ({ path, isWfapi }) => {
+      if (!this.username || !this.apiKey || !this.jenkinsUrl) {
         try {
           const jenkinsAPISettingsMessage = await webex.attachmentActions.get(
             jenkinsAPISettingsEnvelopeID
           );
           this.username = jenkinsAPISettingsMessage.inputs.username;
           this.apiKey = jenkinsAPISettingsMessage.inputs.apiKey;
-        } catch (error) {
-          console.log(error);
-        }
+          this.jenkinsUrl = jenkinsAPISettingsMessage.inputs.jenkinsUrl;
+        } catch (error) {}
       }
 
-      const res = await fetch(path + 'api/json', {
+      const route = isWfapi ? 'wfapi' : 'api/json';
+
+      const res = await fetch(this.jenkinsUrl + path + route, {
         headers: {
           Authorization:
             'Basic ' +
@@ -36,13 +46,13 @@ export default class JenkinsRestAPIService {
         }
       });
       const data = await res.json();
+
       return { data, status: res.status };
     };
   }
 
   async getBuildData(buildUrl): Promise<JenkinsBuildData> {
-    const { data } = await this.jenkinsAPI({ path: buildUrl });
-
+    const { data } = await this.jenkinsAPI({ path: buildUrl, isWfapi: false });
     const commits: Commit[] = data.changeSet?.items?.map((changeSetItem) => {
       return {
         sha: changeSetItem.id,
@@ -55,5 +65,14 @@ export default class JenkinsRestAPIService {
     return {
       commits
     };
+  }
+
+  async getBuildDataTheOtherWay(buildUrl): Promise<JenkinsOtherWayBuildData> {
+    const { data } = await this.jenkinsAPI({
+      path: buildUrl,
+      isWfapi: true
+    });
+
+    return data.stages.some((stage) => stage.status === 'FAILED');
   }
 }
