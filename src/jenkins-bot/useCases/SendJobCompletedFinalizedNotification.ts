b32c1de5ef16eaa08c6fa92a98b76d28a81db65e
ibarczew@cisco.com <ian.barczewski@wwt.com>
2021-12-16 10:46:09 -0600

    SPARK-287498 - created a finalized use case - IB

---
+++ src/jenkins-bot/useCases/SendJobCompletedFinalizedNotification.ts
@@ -0,0 +1,32 @@
+import { Bot } from '../../common/Bot';
+import JobCompletedTemplate, {
+  JobCompletedTemplateData
+} from '../templates/JobCompletedTemplate';
+import { JenkinsJobStatus } from './JenkinsJobStatus';
+import { JobCompletedNotificationDTO } from './JobCompletedNotificationDTO';
+
+export default class SendJobCompletedFinalizedNotificationUseCase {
+  private template: JobCompletedTemplate;
+  constructor(template: JobCompletedTemplate) {
+    this.template = template;
+  }
+  async execute(request: JobCompletedNotificationDTO, bot: Bot) {
+    try {
+      const data: JobCompletedTemplateData = {
+        jobStatus: JenkinsJobStatus.SUCCESS,
+        buildNumber: request.buildNumber,
+        buildURL: request.buildURL,
+        commits: request.commits,
+        jobName: request.jobName,
+        numberOfChanges: request.numberOfGitChanges,
+        scm: request.repoName,
+        scmURL: request.repoURL
+      };
+
+      const jobCompletedCard = this.template.buildCard(data);
+      bot.sendCard(jobCompletedCard);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
